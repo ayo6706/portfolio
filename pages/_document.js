8a4f43d92846aa27413efa0f3871ad4f1de3a8dc
Ayomide Onibokun <35877863+ayo6706@users.noreply.github.com>
2025-07-29 14:37:55 +0100

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -10,9 +10,21 @@ class MyDocument extends Document {
         <Head>
           <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,500;0,800;1,400;1,500;1,700&display=swap" rel="stylesheet" />
           <meta name="robots" content="index, follow" />
-          <meta name="robots" content="max-snippet:-1, max-image-preview:large, max-video-preview:-1"/>
+          <meta name="robots" content="max-snippet:-1, max-image-preview:large, max-video-preview:-1" />
           <link rel="canonical" href="https://repotecc.com/" />
           <Script src="https://use.fontawesome.com/a71e5ce1d6.js" />
+          <Script strategy="afterInteractive">
+            {`
+              (function(w,d,s,o,f,js,fjs) {
+                  w[o]=w[o]||function(){(w[o].q=w[o].q||[]).push(arguments)};
+                  js=d.createElement(s),fjs=d.getElementsByTagName(s)[0];
+                  js.id=o;js.src=f;js.async=1;fjs.parentNode.insertBefore(js,fjs);
+              }(window,document,'script','omnichat','https://omnichat-iota.vercel.app/omnichat-widget.js'));
+
+              omnichat('init', {
+                  agentName: 'ayomide'
+              });
+            `}
         </Head>
         <body>
           <Main />
